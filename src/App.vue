4b0bc6bf6baa3abd04a9b27522e4d52e1e7ef546
haruki1953 <kinikun@outlook.com>
2024-03-01 14:57:57 +0800

    引入 Element Plus 组件库

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@
   <div>
     <!-- <router-view></router-view> -->
     我是app
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
   </div>
 </template>
 
