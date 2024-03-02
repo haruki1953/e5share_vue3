63ce7318e22d5242f5a5b494364c733e6f679698
haruki1953 <kinikun@outlook.com>
2024-03-02 17:03:47 +0800

    登录功能

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,3 +2,9 @@ import request from '@/utils/request'
 
 export const authRegisterService = ({ username, password, email }) =>
   request.post('/auth/register', { username, password, email })
+
+export const authLoginByUsernameService = ({ username, password }) =>
+  request.post('/auth/login/username', { username, password })
+
+export const authLoginByEmailService = ({ email, password }) =>
+  request.post('/auth/login/email', { email, password })
--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,10 +1,16 @@
 <script setup>
 import { User, Lock, Message } from '@element-plus/icons-vue'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
-import { authRegisterService } from '@/api/auth.js'
+import {
+  authRegisterService,
+  authLoginByUsernameService,
+  authLoginByEmailService
+} from '@/api/auth.js'
+import { useAuthStore } from '@/stores/index'
+import { useRouter } from 'vue-router'
 
-const isRegister = ref(true)
+const isRegister = ref(false)
 
 // 表单组件
 const form = ref()
@@ -14,7 +20,9 @@ const formModel = ref({
   username: '',
   email: '',
   password: '',
-  repassword: ''
+  repassword: '',
+  // 选择是否为邮箱登录
+  isLoginByEmail: false
 })
 
 // 整个表单的校验规则
@@ -48,8 +56,8 @@ const rules = {
   password: [
     { required: true, message: '请输入密码', trigger: 'blur' },
     {
-      pattern: /^[a-zA-Z0-9_]{1,32}$/,
-      message: '密码必须是 1-32位 的字符，只能包含字母数字下划线',
+      pattern: /^[a-zA-Z0-9_]{6,32}$/,
+      message: '密码必须是 6-32位 的字符，只能包含字母数字下划线',
       trigger: 'blur'
     }
   ],
@@ -65,9 +73,11 @@ const rules = {
       },
       trigger: 'blur'
     }
-  ]
+  ],
+  isLoginByEmail: []
 }
 
+// 注册
 const register = async () => {
   // 注册成功之前，先进行校验，校验成功 → 请求，校验失败 → 自动提示
   await form.value.validate()
@@ -75,6 +85,40 @@ const register = async () => {
   ElMessage.success('注册成功')
   isRegister.value = false
 }
+
+const authStore = useAuthStore()
+const router = useRouter()
+// 登录
+const login = async () => {
+  await form.value.validate()
+  let res
+  // 根据 isLoginByEmail 选择调用的登陆方式
+  if (formModel.value.isLoginByEmail) {
+    res = await authLoginByEmailService(formModel.value)
+  } else {
+    // eslint-disable-next-line no-unused-vars
+    res = await authLoginByUsernameService(formModel.value)
+  }
+  // 保存token
+  authStore.setToken(res.data.token)
+  ElMessage.success('登录成功')
+  // 跳转至根路径
+  router.push('/')
+}
+
+// 点击忘记密码时弹出的消息
+const forgotPasswordMessage = () => {
+  ElMessage('请联系管理员 X / Twitter: @haruki19530615')
+}
+
+watch(isRegister, () => {
+  formModel.value = {
+    username: '',
+    email: '',
+    password: '',
+    repassword: ''
+  }
+})
 </script>
 
 <template>
@@ -139,16 +183,35 @@ const register = async () => {
           </el-link>
         </el-form-item>
       </el-form>
-      <el-form ref="form" size="large" autocomplete="off" v-else>
+      <el-form
+        :model="formModel"
+        :rules="rules"
+        ref="form"
+        size="large"
+        autocomplete="off"
+        v-else
+      >
         <el-form-item>
           <h1>登录</h1>
         </el-form-item>
-        <el-form-item>
-          <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
+        <el-form-item prop="email" v-if="formModel.isLoginByEmail">
+          <el-input
+            v-model="formModel.email"
+            :prefix-icon="Message"
+            placeholder="请输入邮箱"
+          ></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="username" v-else>
+          <el-input
+            v-model="formModel.username"
+            :prefix-icon="User"
+            placeholder="请输入用户名"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
           <el-input
             name="password"
+            v-model="formModel.password"
             :prefix-icon="Lock"
             type="password"
             placeholder="请输入密码"
@@ -156,12 +219,24 @@ const register = async () => {
         </el-form-item>
         <el-form-item class="flex">
           <div class="flex">
-            <el-checkbox>记住我</el-checkbox>
-            <el-link type="primary" :underline="false">忘记密码？</el-link>
+            <el-checkbox v-model="formModel.isLoginByEmail">
+              邮箱登录
+            </el-checkbox>
+            <el-link
+              @click="forgotPasswordMessage"
+              type="primary"
+              :underline="false"
+            >
+              忘记密码？
+            </el-link>
           </div>
         </el-form-item>
         <el-form-item>
-          <el-button class="button" type="primary" auto-insert-space
+          <el-button
+            @click="login"
+            class="button"
+            type="primary"
+            auto-insert-space
             >登录</el-button
           >
         </el-form-item>
@@ -181,7 +256,7 @@ const register = async () => {
   background-color: #fff;
   .bg {
     background:
-      url('@/assets/logo2.png') no-repeat 60% center / 240px auto,
+      // url('@/assets/logo2.png') no-repeat 60% center / 240px auto,
       url('@/assets/login_bg.jpg') no-repeat center / cover;
     border-radius: 0 20px 20px 0;
   }
