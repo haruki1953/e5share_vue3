e36473e102ac94fac27fcc79bc5af3437152ab08
haruki1953 <kinikun@outlook.com>
2024-03-02 09:57:08 +0800

    准备静态结构

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
-<script setup>
-import { useUserStore } from '@/stores/index'
-const userStore = useUserStore()
-</script>
+<script setup></script>
 
 <template>
   <div>
+    <!-- App.vue只需要留一个路由出口 router-view即可 -->
     <router-view></router-view>
-    我是app
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-
-    <p>{{ userStore.token }}</p>
-    <el-button @click="userStore.setToken('Bearer xxx')">登录</el-button>
-    <el-button @click="userStore.removeToken()">退出</el-button>
   </div>
 </template>
 
